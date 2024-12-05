e31f4f5adcd17ebbc6b70c64aa3a3a6e553f91ed
huseyinerenceyhan <huseyinerenceyhan@gmail.com>
2024-12-05 13:13:23 +0300

    print and update schedule methods

--- SISAO/src/main/java/team5/sisao/Schedule.java
+++ SISAO/src/main/java/team5/sisao/Schedule.java
@@ -2,17 +2,18 @@ package team5.sisao;
 
 public class Schedule {
 
-    private String[][] weeklyProgram ;
+    private String[][] weeklyProgram;
 
-    public Schedule(String[][] weeklyProgram){
-        this.weeklyProgram =weeklyProgram;
+    public Schedule(String[][] weeklyProgram) {
+        this.weeklyProgram = weeklyProgram;
     }
-    public Schedule(){
+
+    public Schedule() {
         this.weeklyProgram = new String[7][16];
 
-        for(int i =0;i<7;i++){
-            for(int x =0;x<16;x++){
-                weeklyProgram[i][x]="";
+        for (int i = 0; i < 7; i++) {
+            for (int x = 0; x < 16; x++) {
+                weeklyProgram[i][x] = "";
             }
         }
 
@@ -21,5 +22,28 @@ public class Schedule {
 
     public String[][] getWeeklyProgram() {
         return weeklyProgram;
+    }
+
+    public Schedule updateSchedule(Schedule schedule, int day, int hour, String word) {
+        String[][] current = schedule.getWeeklyProgram();
+        current[day][hour] = word;
+        schedule.setWeeklyProgram(current);
+        return schedule;
+    }
+
+
+    public void setWeeklyProgram(String[][] weeklyProgram) {
+        this.weeklyProgram = weeklyProgram;
+    }
+
+    public void schdulePrint(Schedule schedule) {
+
+        String[][] weeklyProgram = schedule.getWeeklyProgram();
+        for (int day = 0; day < 7; day++) {
+            for (int hour = 0; hour < 16; hour++) {
+                System.out.print(weeklyProgram[day][hour] + " ");
+            }
+            System.out.println();
+        }
     }
 }
