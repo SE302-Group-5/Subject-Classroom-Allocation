98edd3b86e846a8640bcac91758fc3b30a3d974f
egeakin458 <egeakin458@outlook.com>
2024-12-19 13:31:45 +0300

    CourseManager Updated
    class dosyaları problem yaratıyor

--- SISAO/src/main/java/team5/sisao/CourseManager.java
+++ SISAO/src/main/java/team5/sisao/CourseManager.java
@@ -4,26 +4,26 @@ import java.util.ArrayList;
 
 public class CourseManager {
 
-    private static DatabaseManager db;
+
+    public ScheduleManager sc;
 
     public Course addCourse(String courseName, String day, String hour, int duration, String lecturer, String classroom, ArrayList<String> attendees) {
 
 
         Course course = new Course(courseName, day, hour, duration, lecturer, classroom, attendees);
-
-        db.createEnrollmentTable(course);
-        db.addCourse(course);
+        sc.getDatabaseManager().createEnrollmentTable(course);
+        sc.getDatabaseManager().addCourse(course);
 
         ArrayList<String> list = course.getAttandees();
         for (String student : list) {
-            db.updateSchedule(student, course.getCourseName(), course.getDay(), course.getStartHour(), course.getDuration());
+            sc.getDatabaseManager().updateSchedule(student, course.getCourseName(), course.getDay(), course.getStartHour(), course.getDuration());
         }
         return course;
 
     }
 
     public CourseManager(DatabaseManager db) {
-        this.db = db;
+        sc = new ScheduleManager(db);
     }
 
 }
