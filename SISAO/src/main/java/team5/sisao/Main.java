3ae7111b9941f048d351e7ef5d8c6d68d41a11e0
huseyinerenceyhan <huseyinerenceyhan@gmail.com>
2024-12-21 18:52:18 +0300

    fxml width changed to 1000

--- SISAO/src/main/java/team5/sisao/Main.java
+++ SISAO/src/main/java/team5/sisao/Main.java
@@ -27,9 +27,9 @@ public class Main extends Application {
             Parent root = FXMLLoader.load(location);
             Scene scene = new Scene(root);
             stage.setMaxHeight(800);
-            stage.setMaxWidth(1100);
+            stage.setMaxWidth(1000);
             stage.setMinHeight(800);
-            stage.setMinWidth(1100);
+            stage.setMinWidth(1000);
             stage.setScene(scene);
             stage.setTitle("SISAO");
             stage.show();
